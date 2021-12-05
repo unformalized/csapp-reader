c496b8b6abb4739b50f1f89393f79a538509344f
bestlb <bestmasterliubin@gmail.com>
2021-12-05 15:25:23 +0800

    feat: signal handle

--- book_contents/exception_control_flow/sig_block_test.c
+++ book_contents/exception_control_flow/sig_block_test.c
@@ -1,8 +1,11 @@
 #include "csapp.h"
+#include <bits/types/sigset_t.h>
+
+// 显示阻塞信号测试
 
 void sigint_handler(int sig)
 {
-  printf("Catch SIGINT\n");
+  printf("Catch SIGINT");
   return;
 }
 
@@ -16,13 +19,23 @@ unsigned int snooze(unsigned int secs) {
 int main(int argc, char const *argv[])
 {
   int seconds = 0;
+  sigset_t mask, prev_mask;
   if (argc >= 2) {
     seconds = atoi(argv[1]);
+  } else {
+    seconds = 5;
   }
+  Sigemptyset(&mask);
+  // 设置 sigint 信号阻塞
+  Sigaddset(&mask, SIGINT);
+  Sigprocmask(SIG_BLOCK, &mask, &prev_mask);
 
+  // 注册 sigint 信号处理
   if (signal(SIGINT, sigint_handler) == SIG_ERR)
     unix_error("Signal error\n");
   snooze(seconds);
+
+  Sigprocmask(SIG_SETMASK, &prev_mask, NULL);
 
   return 0;
 }
--- book_contents/exception_control_flow/test_snooze2.c
+++ book_contents/exception_control_flow/test_snooze2.c
@@ -1,8 +1,18 @@
 #include "csapp.h"
+#include <sys/types.h>
+#include <unistd.h>
 
 void sigint_handler(int sig)
 {
-  printf("Catch SIGINT\n");
+  pid_t pid = getpid();
+  printf("current process id: %d\n", pid);
+  printf("Catch SIGINT");
+  /* wait the SIGTERM signal  */
+  return;
+}
+
+void sigterm_handler(int sig) {
+  printf("Catch SIGTERM");
   return;
 }
 
@@ -19,13 +29,14 @@ int main(int argc, char const *argv[])
   if (argc >= 2) {
     seconds = atoi(argv[1]);
   }
+  pid_t pid = getpid();
+  printf("current process id: %d\n", pid);
 
   if (signal(SIGINT, sigint_handler) == SIG_ERR)
+    unix_error("Signal error\n");
+  if (signal(SIGTERM, sigterm_handler) == SIG_ERR)
     unix_error("Signal error\n");
   snooze(seconds);
 
   return 0;
 }
-
-
-
