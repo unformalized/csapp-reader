e7248da8970b939638deb2f92ab6fc3c915fbc41
bestmasterliubin <bestmasterliubin@gmail.com>
2022-12-22 12:53:25 +0800

    feat: static link

--- csapp_internal_impl/src/linker/parseElf.c
+++ csapp_internal_impl/src/linker/parseElf.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
 #include <assert.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <headers/linker.h>
 #include <headers/common.h>
+#include <headers/linker.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-static int parse_table_entry(char *str, char ***ent)
-{
+static int parse_table_entry(char *str, char ***ent) {
     // parse line as table entries
     int count_col = 1;
     int len = strlen(str);
 
-    for (int i = 0; i < len; ++i)
-    {
+    for (int i = 0; i < len; ++i) {
         if (str[i] == ',')
             count_col++;
     }
@@ -25,15 +23,12 @@ static int parse_table_entry(char *str, char ***ent)
     int col_width = 0;
     char col_buf[32];
 
-    for (int i = 0; i < len + 1; ++i)
-    {
-        if (str[i] == ',' || str[i] == '\0')
-        {
+    for (int i = 0; i < len + 1; ++i) {
+        if (str[i] == ',' || str[i] == '\0') {
             assert(col_index < count_col);
             // malloc and copy
             char *col = malloc((col_width + 1) * sizeof(char));
-            for (int j = 0; j < col_width; ++j)
-            {
+            for (int j = 0; j < col_width; ++j) {
                 col[j] = col_buf[j];
             }
             col[col_width] = '\0';
@@ -42,9 +37,7 @@ static int parse_table_entry(char *str, char ***ent)
             arr[col_index] = col;
             col_index++;
             col_width = 0;
-        }
-        else
-        {
+        } else {
             assert(col_width < 32);
             col_buf[col_width] = str[i];
             col_width++;
@@ -54,16 +47,14 @@ static int parse_table_entry(char *str, char ***ent)
     return count_col;
 }
 
-static void free_table_entry(char **ent, int n)
-{
+static void free_table_entry(char **ent, int n) {
     for (int i = 0; i < n; ++i)
         free(ent[i]);
     free(ent);
 }
 
 // parse section header
-static void parse_sh(char *str, sh_entry_t *sh)
-{
+static void parse_sh(char *str, sh_entry_t *sh) {
     char **cols;
     int col_num = parse_table_entry(str, &cols);
     assert(col_num == 4);
@@ -77,20 +68,14 @@ static void parse_sh(char *str, sh_entry_t *sh)
     free_table_entry(cols, col_num);
 }
 
-static void print_sh_entry(sh_entry_t *sh)
-{
-    debug_printf(DEBUG_LINKER,
-                 "%s\t%x\t%d\t%d\n",
-                 sh->sh_name,
-                 sh->sh_addr,
-                 sh->sh_offset,
-                 sh->sh_size);
+static void print_sh_entry(sh_entry_t *sh) {
+    debug_printf(DEBUG_LINKER, "%s\t%x\t%d\t%d\n", sh->sh_name, sh->sh_addr,
+                 sh->sh_offset, sh->sh_size);
 }
 
 // parse symbol table
 // el: sum,STB_GLOBAL,STT_FUNCTION,.text,0,22
-static void parse_sym_table(char *str, st_entry_t *ste)
-{
+static void parse_sym_table(char *str, st_entry_t *ste) {
     char **cols;
     int col_num = parse_table_entry(str, &cols);
     assert(col_num == 6);
@@ -99,39 +84,25 @@ static void parse_sym_table(char *str, st_entry_t *ste)
     strcpy(ste->st_name, cols[0]);
 
     // select symbol bind
-    if (strcmp(cols[1], "STB_GLOBAL") == 0)
-    {
+    if (strcmp(cols[1], "STB_GLOBAL") == 0) {
         ste->bind = STB_GLOBAL;
-    }
-    else if (strcmp(cols[1], "STB_LOCAL") == 0)
-    {
+    } else if (strcmp(cols[1], "STB_LOCAL") == 0) {
         ste->bind = STB_LOCAL;
-    }
-    else if (strcmp(cols[1], "WEAK") == 0)
-    {
+    } else if (strcmp(cols[1], "WEAK") == 0) {
         ste->bind = STB_WEAK;
-    }
-    else
-    {
+    } else {
         printf("symbol bind is neither LOCAL, GLOBAL, not WEAK\n");
         exit(-1);
     }
 
     // select symbol type
-    if (strcmp(cols[2], "STT_NOTYPE") == 0)
-    {
+    if (strcmp(cols[2], "STT_NOTYPE") == 0) {
         ste->type = STT_NOTYPE;
-    }
-    else if (strcmp(cols[2], "STT_FUNC") == 0)
-    {
+    } else if (strcmp(cols[2], "STT_FUNC") == 0) {
         ste->type = STT_FUNC;
-    }
-    else if (strcmp(cols[2], "STT_OBJECT") == 0)
-    {
+    } else if (strcmp(cols[2], "STT_OBJECT") == 0) {
         ste->type = STT_OBJECT;
-    }
-    else
-    {
+    } else {
         printf("symbol type is neither NOTYPE, FUNC, not OBJECT\n");
         exit(-1);
     }
@@ -143,24 +114,17 @@ static void parse_sym_table(char *str, st_entry_t *ste)
     free_table_entry(cols, col_num);
 }
 
-static void print_symbol_entry(st_entry_t *ste)
-{
-    debug_printf(DEBUG_LINKER, "%s\t%d\t%d\t%s\t%d\t%d\n",
-                 ste->st_name,
-                 ste->bind,
-                 ste->type,
-                 ste->st_shndx,
-                 ste->st_value,
+static void print_symbol_entry(st_entry_t *ste) {
+    debug_printf(DEBUG_LINKER, "%s\t%d\t%d\t%s\t%d\t%d\n", ste->st_name,
+                 ste->bind, ste->type, ste->st_shndx, ste->st_value,
                  ste->st_size);
 }
 
-static int read_elf(const char *filename, uint64_t buf_addr)
-{
+static int read_elf(const char *filename, uint64_t buf_addr) {
     // open file and read
     FILE *fp;
     fp = fopen(filename, "r");
-    if (fp == NULL)
-    {
+    if (fp == NULL) {
         debug_printf(DEBUG_LINKER, "unable to open file %s\n", filename);
         exit(1);
     }
@@ -169,42 +133,37 @@ static int read_elf(const char *filename, uint64_t buf_addr)
     char line[MAX_ELF_FILE_WIDTH];
     int line_counter = 0;
 
-    while (fgets(line, MAX_ELF_FILE_WIDTH, fp) != NULL)
-    {
+    while (fgets(line, MAX_ELF_FILE_WIDTH, fp) != NULL) {
         int len = strlen(line);
         if ((len == 0) ||
-            (len >= 1 && (line[0] == '\n' || line[0] == '\r' || line[0] == '\t')) ||
-            (len >= 2 && (line[0] == '/' && line[1] == '/')))
-        {
+            (len >= 1 &&
+             (line[0] == '\n' || line[0] == '\r' || line[0] == '\t')) ||
+            (len >= 2 && (line[0] == '/' && line[1] == '/'))) {
             // remove meaningless line
             continue;
         }
 
         // empty line check
         int isEmpty = 1;
-        for (int i = 0; i < len; ++i)
-        {
-            isEmpty = isEmpty && (line[i] == ' ' || line[i] == '\t' || line[i] == '\r');
+        for (int i = 0; i < len; ++i) {
+            isEmpty = isEmpty &&
+                      (line[i] == ' ' || line[i] == '\t' || line[i] == '\r');
         }
         if (isEmpty == 1)
             continue;
 
-        if (line_counter < MAX_ELF_FILE_WIDTH)
-        {
+        if (line_counter < MAX_ELF_FILE_WIDTH) {
             // store this line to buffer[line_counter]
-            uint64_t addr = buf_addr + line_counter * MAX_ELF_FILE_WIDTH * sizeof(char);
+            uint64_t addr =
+                buf_addr + line_counter * MAX_ELF_FILE_WIDTH * sizeof(char);
             char *line_buf = (char *)addr;
 
             int i = 0;
-            while (i < len && i < MAX_ELF_FILE_WIDTH)
-            {
-                if ((line[i] == '\n') ||
-                    (line[i] == '\r') ||
+            while (i < len && i < MAX_ELF_FILE_WIDTH) {
+                if ((line[i] == '\n') || (line[i] == '\r') ||
                     // inline comment
-                    ((i + 1 < len) &&
-                     (i + 1 < MAX_ELF_FILE_WIDTH) &&
-                     line[i] == '/' && line[i + 1] == '/'))
-                {
+                    ((i + 1 < len) && (i + 1 < MAX_ELF_FILE_WIDTH) &&
+                     line[i] == '/' && line[i + 1] == '/')) {
                     break;
                 }
                 line_buf[i] = line[i];
@@ -212,10 +171,9 @@ static int read_elf(const char *filename, uint64_t buf_addr)
             }
             line_buf[i] = '\0';
             line_counter++;
-        }
-        else
-        {
-            debug_printf(DEBUG_LINKER, "elf file %s is too long (>%d)\n", filename, MAX_ELF_FILE_WIDTH);
+        } else {
+            debug_printf(DEBUG_LINKER, "elf file %s is too long (>%d)\n",
+                         filename, MAX_ELF_FILE_WIDTH);
             fclose(fp);
             exit(1);
         }
@@ -227,12 +185,10 @@ static int read_elf(const char *filename, uint64_t buf_addr)
     return line_counter;
 }
 
-void parse_elf(char *filename, elf_t *elf)
-{
+void parse_elf(char *filename, elf_t *elf) {
     assert(elf != NULL);
     int line_count = read_elf(filename, (uint64_t)(&(elf->buffer)));
-    for (int i = 0; i < line_count; ++i)
-    {
+    for (int i = 0; i < line_count; ++i) {
         printf("[%d]\t%s\n", i, elf->buffer[i]);
     }
 
@@ -242,13 +198,11 @@ void parse_elf(char *filename, elf_t *elf)
 
     sh_entry_t *symt_sh = NULL;
 
-    for (int i = 0; i < sh_count; ++i)
-    {
+    for (int i = 0; i < sh_count; ++i) {
         parse_sh(elf->buffer[2 + i], &(elf->sht[i]));
         print_sh_entry(&(elf->sht[i]));
 
-        if (strcmp(elf->sht[i].sh_name, ".symtab") == 0)
-        {
+        if (strcmp(elf->sht[i].sh_name, ".symtab") == 0) {
             // this is the section header for symbol table
             symt_sh = &(elf->sht[i]);
         }
@@ -259,16 +213,14 @@ void parse_elf(char *filename, elf_t *elf)
     // parse symbol table
     elf->symt_count = symt_sh->sh_size;
     elf->symt = malloc(elf->symt_count * sizeof(st_entry_t));
-    for (int i = 0; i < symt_sh->sh_size; ++i)
-    {
+    for (int i = 0; i < symt_sh->sh_size; ++i) {
         int j = i + symt_sh->sh_offset;
         parse_sym_table(elf->buffer[j], &(elf->symt[i]));
         print_symbol_entry(&(elf->symt[i]));
     }
 }
 
-void free_elf(elf_t *elf)
-{
+void free_elf(elf_t *elf) {
     assert(elf != NULL);
 
     free(elf->sht);
---
+++ csapp_internal_impl/src/linker/staticlink.c
@@ -0,0 +1,3 @@
+//
+// Created by liubin on 22-8-7.
+//
